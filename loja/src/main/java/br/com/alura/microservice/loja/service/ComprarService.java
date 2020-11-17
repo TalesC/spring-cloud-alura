50deeaa21ee54c7075a66fb54d25d431d4a387b4
TalesC <talescarvalhopereira@gmail.com>
2020-11-17 09:13:12 -0300

    add Hystrix to loja project

--- loja/src/main/java/br/com/alura/microservice/loja/service/ComprarService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/ComprarService.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import br.com.alura.microservice.loja.client.FornecedorClient;
 import br.com.alura.microservice.loja.controller.dto.CompraDTO;
 import br.com.alura.microservice.loja.model.Compra;
@@ -19,6 +21,7 @@ public class ComprarService {
 	@Autowired
 	private FornecedorClient fornecedorClient;
 	
+	@HystrixCommand(fallbackMethod = "realizaCompraFallback")
 	public Compra realizaCompra(CompraDTO compra) {
 		
 		final String estado =  compra.getEndereco().toString();
@@ -29,8 +32,19 @@ public class ComprarService {
 		LOG.info("realizando um  pedido", estado);
 		InfoPedidoDTO pedido = fornecedorClient.realizaPedido(compra.getItens());
 		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return new Compra(pedido.getId(), pedido.getTempoDePreparo(), info.getEndereco());
 		
+	}
+	
+	public Compra realizaCompraFallback(CompraDTO compra) {
+		return new Compra(null, null, compra.getEndereco().toString());
 	}
 
 }
